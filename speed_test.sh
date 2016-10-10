8506d0c9fb5e1df52a43c527ad0ae25cfe287b26
Kyle <kyle.sims@comcast.net>
2016-10-09 20:08:00 -0600

    Change to use the $USER variable

--- speed_test.sh
+++ speed_test.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
-/home/[USER]/Documents/python/speed_test/venv/bin/python3 /home/[USER]/Documents/python/speed_test/venv/lib/python3.5/site-packages/speedtest_cli.py --simple > /home/[USER]/Documents/python/speed_test/speed.txt
 
-date -u "+%F %T" >> /home/[USER]/Documents/python/speed_test/speed.txt
+/home/$USER/Documents/python/speed_test/venv/bin/python3 /home/$USER/Documents/python/speed_test/venv/lib/python3.5/site-packages/speedtest_cli.py --simple > /home/$USER/Documents/python/speed_test/speed.txt
+
+date -u "+%F %T" >> /home/$USER/Documents/python/speed_test/speed.txt
