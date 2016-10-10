7f68108d1f08b497ea3042873a74fed4c5461881
Kyle <kyle.sims@comcast.net>
2016-10-09 20:09:02 -0600

    Add workaround wrapper as shebang doesn't expand kyle variable

---
+++ speed_test_wrapper.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+# only done since the shebang (#!) doesn't expand $USER
+exec "/home/$USER/Documents/python/speed_test/venv/bin/python3" "/home/$USER/Documents/python/speed_test/speed_test.py"
