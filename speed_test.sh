656869f3e04c772848915bf46b995a54ef25a81c
Kyle <kyle.sims@comcast.net>
2016-10-09 19:12:14 -0600

    Add bash script

---
+++ speed_test.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+/home/[USER]/Documents/python/speed_test/venv/bin/python3 /home/[USER]/Documents/python/speed_test/venv/lib/python3.5/site-packages/speedtest_cli.py --simple > /home/[USER]/Documents/python/speed_test/speed.txt
+
+date -u "+%F %T" >> /home/[USER]/Documents/python/speed_test/speed.txt
